ec1413bbed8d1215272abec1733f0322f7889157
Cory LePage <cory@lepagegroup.ca>
2017-11-22 17:27:22 -0800

    Initial commit

--- LabelPartProperties/Program.cs
+++ LabelPartProperties/Program.cs
@@ -1,15 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.UI;
 
 namespace LabelPartProperties
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
+			try
+			{
+				LabelPartProperties.Run();
+			}
+			catch (Exception ex)
+			{
+				Trace.WriteLine(ex.InnerException + ex.Message + ex.StackTrace);
+			}
+		}
+
+		public static class LabelPartProperties
+		{
+			public static void Run()
+			{
+				var pickedObjects = new Picker().PickObjects(Picker.PickObjectsEnum.PICK_N_PARTS);
+				if (pickedObjects.GetSize() < 1) return;
+
+				while (pickedObjects.MoveNext())
+				{
+					var part = pickedObjects.Current as Part;
+
+					if(part == null)
+						continue;
+
+					var origin = part.GetCoordinateSystem().Origin;
+					var axisX = part.GetCoordinateSystem().AxisX;
+					var normal = axisX.GetNormal();
+					var length = axisX.GetLength();
+
+					double height = 0;
+					part.GetReportProperty("HEIGHT", ref height);
+
+					string topLevel = string.Empty;
+					part.GetReportProperty("TOP_LEVEL", ref topLevel);
+
+					var midPoint = new Point(
+						origin.X + normal.X * length /2,
+						origin.Y + normal.Y * length / 2,
+						origin.Z + normal.Z * length / 2
+						);
+
+					var markPoint = new Point(midPoint);
+					markPoint.Z += height / 2 + 250;
+
+					var profilePoint = new Point(markPoint);
+					profilePoint.Z -= 100;
+
+					var levelPoint = new Point(markPoint);
+					levelPoint.Z -= 200;
+
+					var partMark = part.GetPartMark();
+
+					var drawer = new GraphicsDrawer();
+					var color = new Color(1, 1, 1);
+
+					drawer.DrawText(markPoint, partMark, color);
+					drawer.DrawText(profilePoint, part.Profile.ProfileString, color);
+					drawer.DrawText(levelPoint, topLevel, color);
+
+					drawer.DrawLineSegment(midPoint, markPoint, color);
+				}
+			}
 		}
 	}
 }
